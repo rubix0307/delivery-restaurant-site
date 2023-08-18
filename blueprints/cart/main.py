97494c1e9fb8adc650bfe0851c10b7583f01ec83
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-08-18 11:17:31 +0200

    start of cart construction

--- blueprints/cart/main.py
+++ blueprints/cart/main.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
 from functions.decorators import login_required
-
+from functions.other import get_user_cart_orders
 
 cart = Blueprint('cart', __name__)
 
@@ -9,16 +9,20 @@ cart = Blueprint('cart', __name__)
 @cart.route('/', methods=['GET', 'PUT'])
 @login_required()
 def cart_index():
+    user, cart, order = get_user_cart_orders()
+
     return render_template('base.html')
 
 
 @cart.route('/order', methods=['POST'])
 @login_required()
 def cart_order():
+    user, cart, order = get_user_cart_orders()
     return
 
 
 @cart.route('/add', methods=['POST'])
 @login_required()
 def cart_add():
+    user, cart, order = get_user_cart_orders()
     return
