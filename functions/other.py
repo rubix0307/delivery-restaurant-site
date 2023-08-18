839556c7b644e6343fc24821ae952a838fda6153
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-08-18 19:18:46 +0200

    edit hint

--- functions/other.py
+++ functions/other.py
@@ -1,5 +1,5 @@
 import json
-
+from sqlalchemy.engine.row import Row
 from flask import session
 
 from functions.db import *
@@ -11,9 +11,5 @@ def get_session_user_data():
 def get_user_cart_orders():
     user = get_session_user_data()
     cart = get_or_create(Order, dict(user_id=user['id'], status_id=1))
-
-    try:
-        orders: (Dish, OrderDish.quantity) = db.session.query(Dish, OrderDish.quantity).join(OrderDish).filter(OrderDish.order_id == cart.id).all()
-    except Exception as ex:
-        print(ex)
+    orders: Row = db.session.query(Dish).join(OrderDish).filter(OrderDish.order_id == cart.id).all()
     return user, cart, orders
