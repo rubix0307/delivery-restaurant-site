51b6773bba5886ea6168aa9d8da3d500b83c61bb
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-08-22 14:59:20 +0200

    edit templates

--- functions/other.py
+++ functions/other.py
@@ -8,5 +8,5 @@ from functions.db import *
 def get_user_cart_orders():
     user = session.get('user')
     cart = get_or_create(Order, dict(user_id=user['id'], status_id=1))
-    orders: Row = db_session.query(Dish, OrderDish.quantity).join(OrderDish).filter(OrderDish.order_id == cart.id).all()
+    orders: Row = db_session.query(Dish, OrderDish).join(OrderDish).filter(OrderDish.order_id == cart.id).all()
     return user, cart, orders
